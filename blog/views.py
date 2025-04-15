ae9a942a222c87afc354e87e8378e7265870e745
younghun <tkdak2011@naver.com>
2025-04-15 15:37:43 +0900

    CBV로 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #template_name = 'blog/index.html'
 
-def index(requset):
-    posts = Post.objects.all().order_by('-pk')
+# def index(requset):
+#    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        requset,
-        'blog/index.html',
-        {
-            'posts': posts, 
-        }
-    )
+#     return render(
+#         requset,
+#         'blog/index.html',
+#         {
+#             'posts': posts, 
+#         }
+#     )  
 
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        "blog/single_post_page.html",
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request,pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         "blog/single_post_page.html",
+#         {
+#             'post': post,
+#         }
+#     )
 # Create your views here.
