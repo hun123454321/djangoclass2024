4b98305aeb3c4962c9a9a20a2688db582e63a862
younghun <tkdak2011@naver.com>
2025-05-13 17:45:23 +0900

    10주차 슬라이스 23

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from .models import Post, Category
 
 from .models import Post
 
 admin.site.register(Post)
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name', )}
+
+admin.site.register(Category,CategoryAdmin)
